647edea54f0389a294db9e607466478b56a1b8e3
Wim Vandeweyer <wvandeweyer@gmail.com>
2024-02-08 12:58:58 +0100

    modified dotfiles

--- fresh.sh
+++ fresh.sh
@@ -26,12 +26,6 @@ brew update
 brew tap homebrew/bundle
 brew bundle --file ./Brewfile
 
-# Set Iterm profile
-# Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2_profile"
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
-
 # Set default MySQL root password and auth type
 mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
