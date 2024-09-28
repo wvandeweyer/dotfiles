55da0b80b42c0c4995205ed3a89b3083bb78f925
Wim Vandeweyer <wvandeweyer@gmail.com>
2024-09-28 11:57:36 +0200

    update site paths

--- clone.sh
+++ clone.sh
@@ -6,7 +6,6 @@ CODE=$HOME/Code
 SITES=$HOME/Herd
 
 # Sites
-git clone git@github.com:wvandeweyer/invoices.git $SITES/portal.2000watt.be
-git clone git@github.com:wvandeweyer/ocumedi.be.git $SITES/ocumedi.be
-git clone git@github.com:wvandeweyer/2000watt.git $SITES/2000watt.be
-git clone git@github.com:wvandeweyer/sharedbouquet.be.git $SITES/sharedbouquet.be
+git clone git@github.com:wvandeweyer/invoices.git $SITES/portal
+git clone git@github.com:wvandeweyer/ocumedi.be.git $SITES/ocumedi
+git clone git@github.com:wvandeweyer/2000watt.git $SITES/2000watt
